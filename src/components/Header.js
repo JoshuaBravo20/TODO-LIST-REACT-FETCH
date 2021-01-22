2fff1f553a9106410154771706ac70960f9e0bc0
JoshuaBravo20 <69777229+JoshuaBravo20@users.noreply.github.com>
2021-01-22 15:51:39 -0300

    succesful-react-functioning

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <div className="header animate__animated animate__bounceInDown animate__fast">
+        <p id="heading">
+           To-Do List
+           
+          <i
+            className="fas fa-sticky-note animate__animated animate__bounceInDown"
+            id="sticky"
+          ></i>
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Header;
---
+++ src/components/MainContainer.js
@@ -0,0 +1,71 @@
+import React, { useState, useRef } from "react";
+
+const MainContainer = () => {
+  let formRef = useRef(null);
+
+  const [taskList, setTaskList] = useState([]);
+
+  function addNewTask(e) {
+    if (e.keyCode === 13 && formRef.value !== "") {
+      setTaskList(taskList.concat(formRef.value));
+      emptyLiRef.current.className = "hiding";
+      formRef.value = "";
+    }
+  }
+
+  function deleteTask(i) {
+    taskList.splice(i, 1);
+    setTaskList([...taskList]);
+  }
+
+  let emptyLiRef = useRef(null);
+
+  return (
+    <>
+      <ul
+        className="list-group animate__animated animate__bounceInUp animate__fast"
+        id="box"
+      >
+        <div className="input-group flex-nowrap input-group-lg">
+          <div className="input-group-prepend"></div>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="New Task"
+            aria-label="task"
+            aria-describedby="addon-wrapping"
+            id="taskLabel"
+            ref={(r) => (formRef = r)}
+            onKeyUp={addNewTask}
+          />
+        </div>
+        <li className="list-group-item text-success" ref={emptyLiRef}>
+          No tasks yet...
+        </li>
+        {!!taskList !== null &&
+          taskList.map((valor, i) => {
+            return (
+              <li
+                key={i}
+                className="list-group-item animate__animated animate__backInDown animate__faster"
+              >
+                {valor}{" "}
+                <i
+                  className="fas fa-trash"
+                  id="trash"
+                  onClick={() => {
+                    deleteTask(i);
+                  }}
+                ></i>
+              </li>
+            );
+          })}
+      </ul>
+      <div id="counter" className="animate__animated animate__bounceInUp">
+        {taskList.length} tasks pending
+      </div>
+    </>
+  );
+};
+
+export default MainContainer;
